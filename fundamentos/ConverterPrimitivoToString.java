8b59a30eeb9ee4079d0ce6ce427250f3de32e6b8
fenix <fenix.azul09@protonmail.com>
2022-08-25 18:22:34 -0300

    src

---
+++ fundamentos/ConverterPrimitivoToString.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class ConverterPrimitivoToString {
+	
+	public static void main(String[] args) {
+		
+		// Wrapper Integer para receber o Tipo Primitivo
+		Integer num1 = 10000;
+		System.out.println(num1.toString().length());
+		
+		// Converter direto na saida mas sempre colocando o wrapper
+		int num2 = 100;
+		System.out.println(Integer.toString(num2).length());
+		
+		// transformar o tipo primitivo em string concatenando com "" +
+		System.out.println(("" + num1).toString().length());
+	}
+
+}
---
+++ treinoformulas/Treino.java
@@ -0,0 +1,16 @@
+package treinoformulas;
+
+import java.util.Scanner;
+
+public class Treino {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Bom dia ");
+		String frase = entrada.nextLine();
+		
+		System.out.println(frase);
+		
+		entrada.close();
+	}
+}
