386672390f576d77e1f1cbfde0b4195566658302
fenix <fenix.azul09@protonmail.com>
2022-09-06 15:22:53 -0300

    src

---
+++ controle/IfElse.java
@@ -0,0 +1,22 @@
+package controle;
+
+import javax.swing.JOptionPane;
+
+public class IfElse {
+	
+	public static void main(String[] args) {
+		
+		// IF/ELSE
+		String valor = JOptionPane.showInputDialog("Escreva o Número: ");
+		
+		// Converte o valor em int
+		int resultado = Integer.parseInt(valor);
+		
+		// se o valor em módular for igual 1 impar entaõ true / else false
+		if (resultado % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("IMPAR");
+		}
+	}
+}
