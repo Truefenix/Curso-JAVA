682cd6b1fc4d9c0b448d5f699d7b75e7615de4dd
fenix <fenix.azul09@protonmail.com>
2022-09-10 18:08:22 -0300

    src

---
+++ controle/DoWhile.java
@@ -0,0 +1,24 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+
+		String texto = "";
+		
+		do {
+			System.out.println("Escreva a Palavra Certa");
+			System.out.print("Quer sair? ");
+			
+			texto = entrada.next();
+		} while(!texto.equalsIgnoreCase("sim"));
+		
+		System.out.println("Você Saiu Parabéns!!!");
+
+		entrada.close();
+	}
+}
