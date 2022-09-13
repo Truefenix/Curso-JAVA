562acf631096fa0f10f80a078a5244cb30ac03aa
fenix <fenix.azul09@protonmail.com>
2022-09-13 13:04:35 -0300

    src

---
+++ zDesafiosTreinos/For/TreinoForSemNumero.java
@@ -0,0 +1,11 @@
+package zDesafiosTreinos.For;
+
+public class TreinoForSemNumero {
+
+	public static void main(String[] args) {
+		
+		for(String a = "#"; !a.equals("######"); a += "#") {
+			System.out.println(a);
+		}
+	}
+}
