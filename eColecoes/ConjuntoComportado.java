a187bb9a4400f5cc3a7b022fe43ef4ec68d502fe
fenix <fenix.azul09@protonmail.com>
2022-10-20 23:04:15 -0300

    src

---
+++ eColecoes/ConjuntoComportado.java
@@ -0,0 +1,17 @@
+package eColecoes;
+import java.util.HashSet;
+
+public class ConjuntoComportado {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> lista = new HashSet<>();
+		lista.add("Carla");
+		lista.add("Eduardo");
+		lista.add("Pedro");
+		
+		for(String comportado: lista) {
+			System.out.println(comportado);
+		}
+	}
+}
