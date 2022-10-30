cdb4b060617b9be7d8d5116a3818315b6d851f67
fenix <fenix.azul09@protonmail.com>
2022-10-29 21:21:15 -0300

    src

---
+++ zDesafiosTreinos/Colecoes/Comida/comida.java
@@ -0,0 +1,31 @@
+package zDesafiosTreinos.Colecoes.Comida;
+
+import javax.swing.JOptionPane;
+
+public class comida {
+
+	public static void main(String[] args) {
+
+		usuario comer = new usuario();
+		
+		String comida = comer.nome;
+		double peso = comer.peso;
+		
+		String senha = "";
+		while (!senha.equals("-1")) {
+
+			comida = JOptionPane.showInputDialog("Oque você quer comer?");
+			String pesoComida = JOptionPane.showInputDialog("qual o peso da comida?");
+			
+			System.out.printf("Comida %s com o peso de %s kg.\n", comida, pesoComida);
+			
+			String saida = JOptionPane.showInputDialog("SAIDA: -1?");
+			
+			peso = Double.parseDouble(pesoComida);
+			
+			senha = saida;
+		}
+		
+
+	}
+}
---
+++ zDesafiosTreinos/Colecoes/Comida/usuario.java
@@ -0,0 +1,16 @@
+package zDesafiosTreinos.Colecoes.Comida;
+
+public class usuario {
+
+	String nome;
+	double peso;
+	
+	usuario() {
+		
+	}
+	
+	usuario(String comida, double peso) {
+		this.nome = comida;
+		this.peso = peso;
+	}
+}
