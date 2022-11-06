7e85285006c3e570df61f21097d8670dcca99f95
fenix <fenix.azul09@protonmail.com>
2022-11-06 11:10:06 -0300

    src

---
+++ eColecoes/Pilha.java
@@ -0,0 +1,23 @@
+package eColecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		
+		Deque<String> livros = new ArrayDeque<>();
+		
+		livros.add("livro 1");
+		livros.push("livro 2");
+		livros.push("livro 3");
+		
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+	}
+}
