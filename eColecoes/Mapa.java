5fe77802d052880e3fa16b0248201705820d9d37
fenix <fenix.azul09@protonmail.com>
2022-11-07 11:56:33 -0300

    src

---
+++ eColecoes/Mapa.java
@@ -0,0 +1,8 @@
+package eColecoes;
+
+public class Mapa {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- eColecoes/Pilha.java
+++ eColecoes/Pilha.java
@@ -13,11 +13,30 @@ public class Pilha {
 		livros.push("livro 2");
 		livros.push("livro 3");
 		
+		// saida de elementos
 		System.out.println(livros.peek());
-		System.out.println(livros.element());
+		System.out.println(livros.element() + "\n");
 		
+		// saida dos elementos conforme a sequência
+		System.out.println(livros.pop());
 		System.out.println(livros.poll());
-		System.out.println(livros.poll());
-		System.out.println(livros.poll());
+		System.out.println(livros.poll()  + "\n");
+		
+		livros.push("livro 2");
+		System.out.println(livros.remove("livro 2") + "\n");
+		
+		// livros.size();
+		// livros.clear();
+		// livros.contains();
+		// livros.isEmpty();
+		
+		livros.add("livro A");
+		livros.push("livro B");
+		livros.push("livro C");
+		
+		// Foreach da Pilha
+		for(String livro: livros) {
+			System.out.println(livro);
+		}
 	}
 }
