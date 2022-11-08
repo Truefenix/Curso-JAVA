1531f4d2dc5e02634629dcc0bb6f8e5c7820fe92
fenix <fenix.azul09@protonmail.com>
2022-11-08 11:43:45 -0300

    src

--- eColecoes/Mapa.java
+++ eColecoes/Mapa.java
@@ -7,6 +7,23 @@ public class Mapa {
 
 	public static void main(String[] args) {
 		
-		Map<Integer, String> usuario = new HashMap<Integer, String>();
+		Map<Integer, String> usuarios = new HashMap<Integer, String>();
+		
+		usuarios.put(1, "Valor 0"); // colocar valor e chave.
+		usuarios.put(1, "Valor 1"); // substitui por ser da mesma chave.
+		usuarios.put(2, "Valor 2");
+		usuarios.put(3, "Valor 3");
+		
+		System.out.println(usuarios.size()); // tamanho
+		System.out.println(usuarios.isEmpty() + "\n");
+		
+		System.out.println(usuarios.keySet()); // todas as chaves
+		System.out.println(usuarios.values()); // todos os valores
+		System.out.println(usuarios.entrySet() + "\n"); // todas as chaves e valores
+		
+		System.out.println(usuarios.containsKey(2));
+		System.out.println(usuarios.containsValue("Valor 3") + "\n");
+		
+		System.out.println(usuarios.get(2)); // pega a chave e retorna o valor
 	}
 }
