fb9beea3bc14484fba02c849a947a22cbc7dcdf9
fenix <fenix.azul09@protonmail.com>
2022-11-08 09:55:42 -0300

    src

--- eColecoes/Mapa.java
+++ eColecoes/Mapa.java
@@ -1,8 +1,12 @@
 package eColecoes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapa {
 
 	public static void main(String[] args) {
 		
+		Map<Integer, String> usuario = new HashMap<Integer, String>();
 	}
 }
