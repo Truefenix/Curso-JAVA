e234ef4d795142cc170a517c05fd82888e9e9f46
Eduardo Roque <94227038+Eduardo-Roque@users.noreply.github.com>
2022-11-08 15:53:28 -0300

    Delete afundamentos directory

--- afundamentos/PrimeiroPrograma/PrimeiroPrograma.java
+++
@@ -1,8 +0,0 @@
-package afundamentos.PrimeiroPrograma;
-
-public class PrimeiroPrograma {
-	public static void main(String[] args) {
-		System.out.println("Primeiro Programa #Parte 01!!!");
-		System.out.println("Primeiro Programa #Parte 02!!!");
-	}
-}
