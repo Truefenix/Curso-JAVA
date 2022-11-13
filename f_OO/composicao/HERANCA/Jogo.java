493298ef7d7ebf32f9d650d4b6891687164b23c9
fenix <fenix.azul09@protonmail.com>
2022-11-13 09:52:48 -0300

    src

--- f_OO/composicao/HERANCA/Jogo.java
+++ f_OO/composicao/HERANCA/Jogo.java
@@ -4,23 +4,23 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		
-		Heroi j1 = new Heroi(); // objeto Heroi
+		Heroi monstro = new Heroi(); // objeto Heroi
 		
 		// Posição de y e x;
-		j1.x = 10;
-		j1.y = 10;
+		monstro.x = 10;
+		monstro.y = 10;
 		
-		Monstro j2 = new Monstro(); // objeto Monstro
+		Monstro heroi = new Monstro(); // objeto Monstro
 		
 		// Posição de y e x;
-		j2.x = 10;
-		j2.y = 11;
+		heroi.x = 10;
+		heroi.y = 11;
 		
 		// Método atacar do Jogador
-		j1.atacar(j2);
-		j2.atacar(j1);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
 		
-		System.out.println(j1.vida); // vida 100 -> 90
-		System.out.println(j2.vida);
+		System.out.println("Monstro tem -> " + monstro.vida);
+		System.out.println("Herói tem -> " + heroi.vida);
 	}
 }
--- f_OO/composicao/HERANCA/Monstro.java
+++ f_OO/composicao/HERANCA/Monstro.java
@@ -3,4 +3,20 @@ package f_OO.composicao.HERANCA;
 public class Monstro extends Jogador {
 
 	// Class que recebeu atributos e comportamentos pelo extends
+
+	// Herança sobrescrita
+	boolean atacar(Jogador oponente) {
+		int deltaX = Math.abs(x - oponente.x);
+		int deltaY = Math.abs(y - oponente.y);
+		
+		if(deltaX == 0 && deltaY == 1) {
+			oponente.vida -= 20; // sobrescrito
+			return true;
+		} else if (deltaX == 1 && deltaY == 0) {
+			oponente.vida -= 20; // sobrescrito
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
